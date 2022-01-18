ba6737c958316c8d1afdc6ffcacde88dab9c092b
Arihant <arihant.tanwar21@gmail.com>
2022-01-18 17:46:46 +0530

    added event listner

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,11 @@
-console.log(1)
-console.log(2)
-setTimeout(()=>
-{
-    console.log('async function called')
-},2000);
-console.log(3)
-console.log(4)
+const request=new XMLHttpRequest();
+
+request.addEventListener('readystatechange',()=>{
+    if(request.readyState===4 && request.status===200)
+    console.log(request.responseText);
+    else if(request.readyState===4)
+    console.log('could not fetch the data');
+});
+
+request.open('GET','https://jsonplaceholder.typicode.com/todos/');
+request.send();
