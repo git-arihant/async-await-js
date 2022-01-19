e2b8e04045fc86bf69de5df195c5022254fc8539
Arihant <arihant.tanwar21@gmail.com>
2022-01-19 10:07:31 +0530

    async-await

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,14 @@
-fetch('todos/luigi.json').then((response)=>{
-    console.log('resolved',response);
-    return response.json();
-}).then(data=>{
-    console.log(data);
-}).catch((err)=>{
-    console.log('rejectd',err);
-});
+const getTodos= async()=>{
+
+    const response=await fetch('todos/luigis.json');
+
+    if(response.status!==200)
+    throw new Error('cannot fetch data');
+
+    const data=await response.json();
+    return data;
+};
+
+getTodos()
+    .then(data => console.log('resolved:',data))
+    .catch(err => console.log('rejected:',err.message));
