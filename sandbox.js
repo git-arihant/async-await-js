91cf2e2578ee9225233b584b352f9c8f965edcd7
Arihant <arihant.tanwar21@gmail.com>
2022-01-19 09:54:08 +0530

    fetch

--- sandbox.js
+++ sandbox.js
@@ -1,30 +1,8 @@
-const getTodods=(resource,callback)=>{
-
-return new Promise((resolve,reject)=>{
-    const request=new XMLHttpRequest();
-
-    request.addEventListener('readystatechange',()=>{
-        if(request.readyState===4 && request.status===200)
-        {
-            const data=JSON.parse(request.responseText);
-            resolve(data);
-        }
-        else if(request.readyState===4)
-        reject('error getting resource');
-    });
-    request.open('GET',resource);
-    request.send();
-});
-};
-
-getTodods('todos/luigi.json').then(data=>{
-    console.log('promise 1 resolved',data);
-    return getTodods('todos/mario.json');
+fetch('todos/luigi.json').then((response)=>{
+    console.log('resolved',response);
+    return response.json();
 }).then(data=>{
-    console.log('promise 2 resolved',data);
-    return getTodods('todos/shaun.json');
-}).then(data=>{
-    console.log('promise 3 resolved',data);
-}).catch(err=>{
-    console.log('promise rejected : ',err);
+    console.log(data);
+}).catch((err)=>{
+    console.log('rejectd',err);
 });
