2152f4e4f6431b7ca5693ceae09ff8b458425950
Arihant <arihant.tanwar21@gmail.com>
2022-01-19 09:39:13 +0530

    promise

--- sandbox.js
+++ sandbox.js
@@ -1,24 +1,30 @@
-const getTodods=(callback)=>{
+const getTodods=(resource,callback)=>{
+
+return new Promise((resolve,reject)=>{
     const request=new XMLHttpRequest();
 
     request.addEventListener('readystatechange',()=>{
         if(request.readyState===4 && request.status===200)
         {
             const data=JSON.parse(request.responseText);
-            callback(undefined,data);
+            resolve(data);
         }
-    else if(request.readyState===4)
-        callback('could not fetch data',undefined);
+        else if(request.readyState===4)
+        reject('error getting resource');
     });
-
-    request.open('GET','https://jsonplaceholder.typicode.com/todos/');
+    request.open('GET',resource);
     request.send();
+});
 };
 
-getTodods((err,data)=>{
-    console.log('callback fired');
-    if(err)
-        console.log(err);
-    else
-        console.log(data);
+getTodods('todos/luigi.json').then(data=>{
+    console.log('promise 1 resolved',data);
+    return getTodods('todos/mario.json');
+}).then(data=>{
+    console.log('promise 2 resolved',data);
+    return getTodods('todos/shaun.json');
+}).then(data=>{
+    console.log('promise 3 resolved',data);
+}).catch(err=>{
+    console.log('promise rejected : ',err);
 });
