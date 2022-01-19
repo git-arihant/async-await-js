4fec8e81c3b7df021a0ef507a07bafad652a4163
Arihant <arihant.tanwar21@gmail.com>
2022-01-19 10:15:48 +0530

    fetch api

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,8 @@
-const getTodos= async()=>{
-
-    const response=await fetch('todos/luigis.json');
-
-    if(response.status!==200)
-    throw new Error('cannot fetch data');
-
-    const data=await response.json();
-    return data;
-};
-
-getTodos()
-    .then(data => console.log('resolved:',data))
-    .catch(err => console.log('rejected:',err.message));
+fetch('todos/luigi.json').then(response=>{
+    console.log('resolvedr',response);
+    return response.json();  //returns promise
+}).then(data=>{
+    console.log(data);
+}).catch(err=>{
+    console.log('error:',err);
+})
